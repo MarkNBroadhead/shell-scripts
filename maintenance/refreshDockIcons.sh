d843d24664d625ca8f54489d9b337282c511d884
Mark N Broadhead <MarkNBroadhead@gmail.com>
2018-01-02 09:27:37 -0700

    Add script to refresh dock icons

---
+++ maintenance/refreshDockIcons.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo find /private/var/folders/ -name com.apple.dock.iconcache -exec rm {} \;
+sudo find /private/var/folders/ -name com.apple.iconservices -exec rm -rf {} \;
+sudo rm -rf /Library/Caches/com.apple.iconservices.store
