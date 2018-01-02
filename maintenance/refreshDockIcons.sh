1993103ca2db13447fff8be580157e80865bb2f8
Mark N Broadhead <MarkNBroadhead@gmail.com>
2018-01-02 11:25:29 -0700

    Add output messages to script

--- maintenance/refreshDockIcons.sh
+++ maintenance/refreshDockIcons.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Removing icon cache"
 sudo find /private/var/folders/ -name com.apple.dock.iconcache -exec rm {} \;
 sudo find /private/var/folders/ -name com.apple.iconservices -exec rm -rf {} \;
 sudo rm -rf /Library/Caches/com.apple.iconservices.store
+echo "Please reboot."
